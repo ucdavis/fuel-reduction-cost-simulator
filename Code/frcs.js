fac0aaf8284a83644ab4bb8efcf41d34e9807032
Kaiyan Li <kkyli@ucdavis.edu>
2019-06-07 11:53:07 -0700

    finish function Processing

--- Code/frcs.js
+++ Code/frcs.js
@@ -247,7 +247,7 @@ function calculate(){
     let CostFellBunch=FellBunch(Slope,RemovalsST,TreeVolST,DBHST,NonSelfLevelCabDummy,CSlopeFB_Harv,CRemovalsFB_Harv,CHardwoodST);
     let CostManFLBLLT=FellLargeLogTrees(Slope,RemovalsLLT,TreeVolLLT,cut_type,DBHLLT,LogsPerTreeLLT);
     let CostSkidBun= Skidding(Slope,deliver_dist,Removals,TreeVol,WoodDensity,LogLength,cut_type,CSlopeSkidForwLoadSize,LogsPerTree,LogVol,ManualMachineSize,BFperCF,ButtDiam);
-    let CostProcess=8.18;
+    let CostProcess=Processing(TreeVolSLT,DBHSLT,ButtDiamSLT,LogsPerTreeSLT,MechMachineSize);
     let CostLoad=7.78;
     let CostChipWT=7.76;
     let MoveInCosts1G39=79.06;
@@ -726,4 +726,78 @@ function Skidding(Slope,YardDist,Removals,TreeVol,WoodDensity,LogLength,PartialC
             +RelevanceSkidIVE*VolPerPMHskidIVE+RelevanceSkidIVF*VolPerPMHskidIVF+RelevanceSkidIVG*VolPerPMHskidIVG+RelevanceSkidIVH*VolPerPMHskidIVH);
 
     return Math.round(CostSkidBun * 100) / 100;
+}
+
+function Processing(TreeVolSLT,DBHSLT,ButtDiamSLT,LogsPerTreeSLT,MechMachineSize){
+    let PMH_ProcessorS=209.64 // hardcoded
+    let PMH_ProcessorB=265.46 // hardcoded
+
+    // Processing Calculated Values
+    let ProcessorHourlyCost;
+    // A) Hahn Stroke Processor (Gonsier&Mandzak, 87)
+    let TimePerTreeProcessA,VolPerPMHProcessA,CostPerCCFprocessA,RelevanceProcessA;
+    // B) Stroke Processor (MacDonald, 90)
+    let TimePerTreeProcessB,VolPerPMHprocessB,CostPerCCFprocessB,RelevanceProcessB;
+    // C) Roger Stroke Processor (Johnson, 88)
+    let TimePerTreeProcessC,VolPerPMHprocessC,CostPerCCFprocessC,RelevanceProcessC;
+    // D) Harricana Stroke Processor (Johnson, 88)
+    let TimePerTreeProcessD,VolPerPMHprocessD,CostPerCCFprocessD,RelevanceProcessD;
+    // E) Hitachi EX150/Keto 500 (Schroder&Johnson, 97)
+    let TimePerTreeProcessE,VolPerPMHprocessE,CostPerCCFprocessE,RelevanceProcessE;
+    // F) FERIC Generic (Gingras, J.F. 96. The cost of product sorting during harvesting. FERIC Technical Note TN-245)
+    let VolPerPMHprocessF,CostPerCCFprocessF,RelevanceProcessF;
+    // G) Valmet 546 Woodstar Processor (Holtzscher, M. and B. Lanford 1997 Tree diameter effects on costs and productivity of cut-to-length systems. For. Prod. J. 47(3):25-30)
+    let TimePerTreeProcessG,VolPerPMHprocessG,CostPerCCFprocessG,RelevanceProcessG;
+    // H) User-Defined
+    let VolPerPMHprocessH,CostPerCCFprocessH,RelevanceProcessH;
+    // Processing Summary
+    let CostProcess;
+
+    // Processing Calculated Values
+    ProcessorHourlyCost=PMH_ProcessorS*(1-MechMachineSize)+PMH_ProcessorB*MechMachineSize;
+    // A) Hahn Stroke Processor (Gonsier&Mandzak, 87)
+    TimePerTreeProcessA=1.26*(0.232+0.0494*DBHSLT);
+    VolPerPMHProcessA=TreeVolSLT/(TimePerTreeProcessA/60);
+    CostPerCCFprocessA=100*ProcessorHourlyCost/VolPerPMHProcessA;
+    RelevanceProcessA=(DBHSLT<15?1:(DBHSLT<20?4-DBHSLT/5:0));
+    // B) Stroke Processor (MacDonald, 90)
+    TimePerTreeProcessB=0.153+0.0145*ButtDiamSLT;
+    VolPerPMHprocessB=TreeVolSLT/(TimePerTreeProcessB/60);
+    CostPerCCFprocessB=100*ProcessorHourlyCost/VolPerPMHprocessB;
+    RelevanceProcessB=(ButtDiamSLT<20?1:(ButtDiamSLT<30?3-ButtDiamSLT/10:0));
+    // C) Roger Stroke Processor (Johnson, 88)
+    TimePerTreeProcessC=-0.05+0.6844*LogsPerTreeSLT+5*Math.pow(10,-8)*Math.pow(TreeVolSLT,2);
+    VolPerPMHprocessC=TreeVolSLT/(TimePerTreeProcessC/60);
+    CostPerCCFprocessC=100*ProcessorHourlyCost/VolPerPMHprocessC;
+    RelevanceProcessC=1;
+    // D) Harricana Stroke Processor (Johnson, 88)
+    TimePerTreeProcessD=-0.13+0.001*Math.pow(ButtDiamSLT,2)+0.5942*LogsPerTreeSLT;
+    VolPerPMHprocessD=TreeVolSLT/(TimePerTreeProcessD/60);
+    CostPerCCFprocessD=100*ProcessorHourlyCost/VolPerPMHprocessD;
+    RelevanceProcessD=1;
+    // E) Hitachi EX150/Keto 500 (Schroder&Johnson, 97)
+    TimePerTreeProcessE=Math.pow(0.67+0.0116*TreeVolSLT,2);
+    VolPerPMHprocessE=TreeVolSLT/(TimePerTreeProcessE/60);
+    CostPerCCFprocessE=100*ProcessorHourlyCost/VolPerPMHprocessE;
+    RelevanceProcessE=(TreeVolSLT<50?1:(TreeVolSLT<100?2-TreeVolSLT/50:0));
+    // F) FERIC Generic (Gingras, J.F. 96. The cost of product sorting during harvesting. FERIC Technical Note TN-245)
+    VolPerPMHprocessF=(41.16/0.02832)*Math.pow(TreeVolSLT/35.31,0.4902);
+    CostPerCCFprocessF=100*ProcessorHourlyCost/VolPerPMHprocessF;
+    RelevanceProcessF=1;
+    // G) Valmet 546 Woodstar Processor (Holtzscher, M. and B. Lanford 1997 Tree diameter effects on costs and productivity of cut-to-length systems. For. Prod. J. 47(3):25-30)
+    TimePerTreeProcessG=-0.341+0.1243*DBHSLT;
+    VolPerPMHprocessG=TreeVolSLT/(TimePerTreeProcessG/60);
+    CostPerCCFprocessG=100*ProcessorHourlyCost/VolPerPMHprocessG;
+    RelevanceProcessG=(TreeVolSLT<20?1:(TreeVolSLT<40?2-TreeVolSLT/20:0));
+    // H) User-Defined
+    VolPerPMHprocessH=0.001;
+    CostPerCCFprocessH=100*ProcessorHourlyCost/VolPerPMHprocessH;
+    RelevanceProcessH=0;
+    // Processing Summary
+    CostProcess=(TreeVolSLT>0?CHardwoodSLT*100*(ProcessorHourlyCost*RelevanceProcessA+ProcessorHourlyCost*RelevanceProcessB+ProcessorHourlyCost*RelevanceProcessC
+        +ProcessorHourlyCost*RelevanceProcessD+ProcessorHourlyCost*RelevanceProcessE+ProcessorHourlyCost*RelevanceProcessF+ProcessorHourlyCost*RelevanceProcessG+ProcessorHourlyCost*RelevanceProcessH)
+        /(RelevanceProcessA*VolPerPMHProcessA+RelevanceProcessB*VolPerPMHprocessB+RelevanceProcessC*VolPerPMHprocessC+RelevanceProcessD*VolPerPMHprocessD
+            +RelevanceProcessE*VolPerPMHprocessE+RelevanceProcessF*VolPerPMHprocessF+RelevanceProcessG*VolPerPMHprocessG+RelevanceProcessH*VolPerPMHprocessH):0);
+
+    return Math.round(CostProcess * 100) / 100; // round to at most 2 decimal places
 }
