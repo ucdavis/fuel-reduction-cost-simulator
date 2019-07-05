3f1f3cfa0adcc6fb1981ff2bc467edb9b48030c5
Kaiyan Li <kkyli@ucdavis.edu>
2019-07-04 22:35:35 -0700

    add cable-manual-wt-log part

--- api/methods/inlimits.ts
+++ api/methods/inlimits.ts
@@ -1,13 +1,12 @@
 // Outputs sheet - Limits part
-import { Limit } from './frcs.model';
+import { InputVarMod, IntermediateVarMod, Limit } from './frcs.model';
 
-function InLimits(system: string, TreeVolCT: number, TreeVolSLT: number, TreeVolLLT: number,
-                  TreeVolALT: number, TreeVol: number, Slope: number, RemovalsLLT: number, RemovalsALT: number) {
+function InLimits(input: InputVarMod, intermediate: IntermediateVarMod) {
     const limit: Limit = { MaxLLTperAcre: 0, MaxLLTasPercentALT: 0, AvgTreeSizeLimit4Chipping: 0,
                            AvgTreeSizeLimit4Processing: 0, AvgTreeSizeLimit4ManualFellLimbBuck: 0,
                            AvgTreeSizeLimit4loading: 0, AvgTreeSize4GrappleSkidding: 0, SlopeLimit: 0,
                            YardingDistLimit: 0 };
-    switch (system) {
+    switch (input.system) {
         case 'Ground-Based Mech WT':
             limit.MaxLLTperAcre = 0;
             limit.MaxLLTasPercentALT = 0;
@@ -52,19 +51,33 @@ function InLimits(system: string, TreeVolCT: number, TreeVolSLT: number, TreeVol
             limit.SlopeLimit = 40;
             limit.YardingDistLimit = 0;
             break;
+        case 'Cable Manual WT/Log':
+            limit.MaxLLTperAcre = 0;
+            limit.MaxLLTasPercentALT = 0;
+            limit.AvgTreeSizeLimit4Chipping = 80;
+            limit.AvgTreeSizeLimit4Processing = 9999; // assign a large number to indicate it has no upper limit
+            limit.AvgTreeSizeLimit4ManualFellLimbBuck = 9999; // assign a large number to indicate it has no upper limit
+            limit.AvgTreeSizeLimit4loading = 250;
+            limit.AvgTreeSize4GrappleSkidding = 250;
+            limit.SlopeLimit = 100;
+            limit.YardingDistLimit = 1300;
+            break;
     }
     const ExceededMaxLLT = (limit.MaxLLTperAcre === 0 && limit.MaxLLTasPercentALT === 0) ? 0
-        : ((RemovalsLLT > limit.MaxLLTperAcre
-            || 100 * RemovalsLLT / (RemovalsALT > 0 ? RemovalsALT : 1) > limit.MaxLLTasPercentALT)
+        : ((input.RemovalsLLT > limit.MaxLLTperAcre
+            || 100 * input.RemovalsLLT / (intermediate.RemovalsALT > 0 ?
+                intermediate.RemovalsALT : 1) > limit.MaxLLTasPercentALT)
             ? 1 : 0);
     const ExceededMaxTreeVol
-    = (TreeVolCT > limit.AvgTreeSizeLimit4Chipping || TreeVolSLT > limit.AvgTreeSizeLimit4Processing
-    || TreeVolLLT > limit.AvgTreeSizeLimit4ManualFellLimbBuck || TreeVolALT > limit.AvgTreeSizeLimit4loading
-    || TreeVol > limit.AvgTreeSize4GrappleSkidding) ? 1 : 0;
+    = (input.TreeVolCT > limit.AvgTreeSizeLimit4Chipping || input.TreeVolSLT > limit.AvgTreeSizeLimit4Processing
+    || input.TreeVolLLT > limit.AvgTreeSizeLimit4ManualFellLimbBuck
+    || intermediate.TreeVolALT > limit.AvgTreeSizeLimit4loading
+    || intermediate.TreeVol > limit.AvgTreeSize4GrappleSkidding) ? 1 : 0;
     // Slope, %
-    const ExceededMaxSkidLimit = Slope > limit.SlopeLimit ? 1 : 0;
+    const ExceededMaxSkidLimit = input.Slope > limit.SlopeLimit ? 1 : 0;
     // Yarding distance, ft
-    const ExceededMaxYardingDist = limit.YardingDistLimit === 0 ? 0 : 1; // todo: else
+    const ExceededMaxYardingDist = limit.YardingDistLimit === 0 ?
+        0 : (input.deliver_dist > limit.YardingDistLimit ? 1 : 0);
     const InLimits1 = (ExceededMaxLLT === 1 || ExceededMaxTreeVol === 1
     || ExceededMaxSkidLimit === 1 || ExceededMaxYardingDist === 1) ? 0 : 1;
 
