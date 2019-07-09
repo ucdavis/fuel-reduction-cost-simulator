8ed946015fe0013c4fe188b6252a152e79985a1b
Kaiyan Li <kkyli@ucdavis.edu>
2019-07-09 11:19:15 -0700

    update swagger

--- api/index.ts
+++ api/index.ts
@@ -1,9 +1,13 @@
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
 
 import { InputVarMod } from './systems/frcs.model';
 import { calculate } from './systems/frcsrun';
+
+// tslint:disable-next-line: no-var-requires
+const swaggerDocument = require('./swagger.json');
 
 dotenv.config();
 
@@ -18,9 +22,10 @@ app.get('/frcsrun', async (req, res) => {
   console.log(req.body);
   const result = await calculate(params);
   console.log(result);
-  res.json(result);
+  res.status(200).json(result);
+  res.status(400).send('Invalid parameters supplied');
 });
 
-app.get('/', (req, res) => res.send('Hello World!!!'));
+app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(port, () => console.log(`Listening on port ${port}!`));
