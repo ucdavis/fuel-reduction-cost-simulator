4c2795ea3274914af5fa8e66230211c1e9d47917
Laura Holstege <laholstege@ucdavis.edu>
2019-09-05 13:26:11 -0700

    oopsies

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,5 @@ import { InputVarMod } from './systems/frcs.model';
 import { calculate } from './systems/frcsrun';
 
 export const runFrcs = (params: InputVarMod) => {
-  calculate(params);
+  return calculate(params);
 };
