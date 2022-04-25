bf7f55df4b8cbae4ccb57391812749a2e26a60f1
Kaiyan Li <38189032+kaiyanl@users.noreply.github.com>
2022-04-25 10:15:16 -0700

    use built-in toFixed in test

--- src/test.ts
+++ src/test.ts
@@ -1,34 +1,28 @@
 import { getFrcsOutputs } from './index';
 import { testGroundMechWt } from './test.data';
 
-// https://stackoverflow.com/questions/11832914/how-to-round-to-at-most-2-decimal-places-if-necessary?noredirect=1&lq=1
-function roundToNdecimal(num: number, n: number) {
-  const x = 10 ** n;
-  return Math.round((num + Number.EPSILON) * x) / x;
-}
-
 test('testGroundMechWt', () => {
   const res = getFrcsOutputs(testGroundMechWt);
 
-  expect(roundToNdecimal(res.total.yieldPerAcre, 8)).toBe(37.87998061);
-  expect(roundToNdecimal(res.total.costPerAcre, 7)).toBe(627.3814992);
-  expect(roundToNdecimal(res.total.costPerBoleCCF, 8)).toBe(63.00680825);
-  expect(roundToNdecimal(res.total.costPerGT, 8)).toBe(16.56235006);
-  expect(roundToNdecimal(res.total.dieselPerAcre, 7)).toBe(12.6485629);
-  expect(roundToNdecimal(res.total.dieselPerBoleCCF, 9)).toBe(1.270272678);
-  expect(roundToNdecimal(res.total.gasolinePerAcre, 9)).toBe(0.289099958);
-  expect(roundToNdecimal(res.total.gasolinePerBoleCCF, 9)).toBe(0.029033795);
-  expect(roundToNdecimal(res.total.jetFuelPerAcre, 0)).toBe(0);
-  expect(roundToNdecimal(res.total.jetFuelPerBoleCCF, 0)).toBe(0);
+  expect(parseFloat(res.total.yieldPerAcre.toFixed(8))).toBe(37.87998061);
+  expect(parseFloat(res.total.costPerAcre.toFixed(7))).toBe(627.3814992);
+  expect(parseFloat(res.total.costPerBoleCCF.toFixed(8))).toBe(63.00680825);
+  expect(parseFloat(res.total.costPerGT.toFixed(8))).toBe(16.56235006);
+  expect(parseFloat(res.total.dieselPerAcre.toFixed(7))).toBe(12.6485629);
+  expect(parseFloat(res.total.dieselPerBoleCCF.toFixed(9))).toBe(1.270272678);
+  expect(parseFloat(res.total.gasolinePerAcre.toFixed(9))).toBe(0.289099958);
+  expect(parseFloat(res.total.gasolinePerBoleCCF.toFixed(9))).toBe(0.029033795);
+  expect(parseFloat(res.total.jetFuelPerAcre.toFixed(0))).toBe(0);
+  expect(parseFloat(res.total.jetFuelPerBoleCCF.toFixed(0))).toBe(0);
 
-  expect(roundToNdecimal(res.residual.yieldPerAcre, 8)).toBe(11.55664724);
-  expect(roundToNdecimal(res.residual.costPerAcre, 7)).toBe(153.2614717);
-  expect(roundToNdecimal(res.residual.costPerBoleCCF, 8)).toBe(15.39177705);
-  expect(roundToNdecimal(res.residual.costPerGT, 9)).toBe(4.045975452);
-  expect(roundToNdecimal(res.residual.dieselPerAcre, 9)).toBe(4.409948082);
-  expect(roundToNdecimal(res.residual.dieselPerBoleCCF, 9)).toBe(0.442883243);
-  expect(roundToNdecimal(res.residual.gasolinePerAcre, 0)).toBe(0);
-  expect(roundToNdecimal(res.residual.gasolinePerBoleCCF, 0)).toBe(0);
-  expect(roundToNdecimal(res.residual.jetFuelPerAcre, 0)).toBe(0);
-  expect(roundToNdecimal(res.residual.jetFuelPerBoleCCF, 0)).toBe(0);
+  expect(parseFloat(res.residual.yieldPerAcre.toFixed(8))).toBe(11.55664724);
+  expect(parseFloat(res.residual.costPerAcre.toFixed(7))).toBe(153.2614717);
+  expect(parseFloat(res.residual.costPerBoleCCF.toFixed(8))).toBe(15.39177705);
+  expect(parseFloat(res.residual.costPerGT.toFixed(9))).toBe(4.045975452);
+  expect(parseFloat(res.residual.dieselPerAcre.toFixed(9))).toBe(4.409948082);
+  expect(parseFloat(res.residual.dieselPerBoleCCF.toFixed(9))).toBe(0.442883243);
+  expect(parseFloat(res.residual.gasolinePerAcre.toFixed(0))).toBe(0);
+  expect(parseFloat(res.residual.gasolinePerBoleCCF.toFixed(0))).toBe(0);
+  expect(parseFloat(res.residual.jetFuelPerAcre.toFixed(0))).toBe(0);
+  expect(parseFloat(res.residual.jetFuelPerBoleCCF.toFixed(0))).toBe(0);
 });
